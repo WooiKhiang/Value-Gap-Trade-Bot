c11d62d5a5bc986ae02b69bd38bcc36ee3fc5b1d
WooiKhiang <wooikhiang@gmail.com>
2026-02-03 20:23:37 +0800

    Create main_signals_to_trades_momentum.py

---
+++ src/main_signals_to_trades_momentum.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from .signals_to_trades_momentum import signals_to_trades_momentum
+
+
+def main():
+    signals_to_trades_momentum()
+
+
+if __name__ == "__main__":
+    main()
